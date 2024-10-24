44ace3bdc7d77617e3e71522a116c53dfbee853f
Filip Zagórski <f.zagorski1909@gmail.com>
2024-10-24 11:39:25 +0200

    3rd commit

--- app/src/main/java/com/example/zad1_zagorski/MainActivity.kt
+++ app/src/main/java/com/example/zad1_zagorski/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val startButton: Button = findViewById(R.id.startButton)
-            startButton.setOnClickListener {
+        startButton.setOnClickListener {
             val intent = Intent(this, QuizActivity::class.java)
             startActivity(intent)
         }
